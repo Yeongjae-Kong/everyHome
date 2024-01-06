16ce389d22c480a987f2e624209f998b5f31e78e
Yeongjae-Kong <kong7300@dgist.ac.kr>
2024-01-06 17:00:28 +0900

    add backend

---
+++ backend/src/routes/user.js
@@ -0,0 +1,92 @@
+const { Router } = require('express');
+const router = Router();
+
+const mysqlConnection = require('../database/database.js');
+
+router.get('/', (req, res) => {
+    res.status(200).json('Server on port 80 and Database is connected.');
+});
+
+router.get('/:users', (req, res) => {
+    mysqlConnection.query('select * from user', (error, rows, fields) => {
+        if(!error) {
+            res.json(rows);
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/:users/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('select * from user where id = ?', [id], (error, rows, fields) => {
+        if(!error) {
+            res.json(rows);
+        } else {
+            console.log(error);
+        }
+    })
+});
+
+router.post('/:users', (req, res) => {
+    const { id, username, name, email, password } = req.body;
+    console.log(req.body);
+    mysqlConnection.query('insert into user(id, username, name, email, password) values (?, ?, ?, ?, ?)', [ id, username, name, email, password], (error, rows, fields) => {
+        if(!error) {
+            res.json({Status : "User saved"})
+        } else {
+            console.log(error);
+        }
+    });
+})
+
+router.put('/:users/:id', (req, res) => {
+    const { id, username, name, email, password} = req.body;
+    console.log(req.body);
+    mysqlConnection.query('update user set username = ?, name = ?, email = ?, password where id = ?;', 
+    [username, name, email, password, id], (error, rows, fields) => {
+        if(!error){
+            res.json({
+                Status: 'User updated'
+            });
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+router.delete('/:users/:id', (req,res) => {
+    const { id } = req.params;
+    mysqlConnection.query('delete from user where id = ?', [id], (error, rows, fields) => {
+        if(!error){
+            res.json({
+                Status: "User deleted"
+            });
+        } else {
+            res.json({
+                Status: error
+            });
+        }
+    })
+});
+
+module.exports = router;
+
+// [{
+// 	"id":1,
+//     "username": "Veterano23",
+//     "name": "Jesus",
+//     "lastname": "Hedo",
+//     "mail": "hedo.jesus@gmail.com",
+//     "randomstr": "dsDsfj=·",
+//     "hash": "sdfjioi93448rfj7"
+// },
+// {
+// 	"id":2,
+//     "username": "Xavito95",
+//     "name": "Xavi",
+//     "lastname": "Castro",
+//     "mail": "xc@gmail.com",
+//     "randomstr": "gHf08vf^",
+//     "hash": "1sr6p5ly´c,wzh6/"
+// }]
---
+++ lib/viewmodel/user_viewmodel.dart
@@ -0,0 +1,98 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:cs496_project2_front_end/model/user_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<List<UserModel>> fetchUsers() async {
+  final response =
+      await http.get(Uri.parse('http://192.249.18.152/user/fetchall'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<dynamic> userList = jsonDecode(response.body);
+    List<UserModel> users = [];
+    for (final user in userList) {
+      users.add(UserModel.fromJson(user));
+    }
+    return users;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Users');
+  }
+}
+
+Future<UserModel> addUser(UserModel user) async {
+  final response = await http.post(
+      Uri.parse('http://192.249.18.152/user/createuser'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, dynamic>{
+        'u_id': user.u_id,
+        'name': user.name,
+        'profile_word': user.profile_word,
+        'profile_pic': user.profile_pic,
+        'email': user.email,
+        'password': user.password,
+        'birthdate': user.birthdate
+      }));
+  if (response.statusCode == 201) {
+    return UserModel.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to add user');
+  }
+}
+
+Future<UserModel?> fetchUserByEmail(String userEmail) async {
+  List<UserModel> users = await fetchUsers();
+
+  for (var user in users) {
+    if (user.email == userEmail) return user;
+  }
+  return null;
+}
+
+Future<UserModel?> fetchUserByUid(int uid) async {
+  List<UserModel> users = await fetchUsers();
+
+  for (var user in users) {
+    if (user.u_id == uid) return user;
+  }
+  return null;
+}
+
+Future<UserModel?> fetchUserByUidWithoutGiven() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String uid = prefs.getString('u_id') ?? '0';
+  List<UserModel> users = await fetchUsers();
+  log('Current user id: ${uid}');
+  for (var user in users) {
+    if (user.u_id == int.parse(uid)) return user;
+  }
+  return null;
+}
+
+Future<UserModel> updateUser(UserModel user) async {
+  final response = await http.put(
+      Uri.parse('http://192.249.18.152/user/updateuser'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, dynamic>{
+        'u_id': user.u_id,
+        'name': user.name,
+        'profile_word': user.profile_word,
+        'profile_pic': user.profile_pic,
+        'email': user.email,
+        'password': user.password,
+        'birthdate': user.birthdate
+      }));
+  if (response.statusCode == 200) {
+    return UserModel.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to update user');
+  }
+}
