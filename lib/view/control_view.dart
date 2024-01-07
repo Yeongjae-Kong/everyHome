962b32e66d219edd21e199d13a3c4291236adad3
Yeongjae-Kong <kong7300@dgist.ac.kr>
2024-01-08 01:27:11 +0900

    01/08 01:26 modify control_view

--- lib/view/control_view.dart
+++ lib/view/control_view.dart
@@ -1,41 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:madcamp_week2/view/CustomAppBar.dart';
+import 'package:madcamp_week2/view/BoardView.dart';
+import 'package:madcamp_week2/view/TopMenuView.dart';
+import 'package:madcamp_week2/view/NoticeView.dart';
 
 class ControlView extends StatefulWidget {
   const ControlView({Key? key}) : super(key: key);
 
   @override
-  State<ControlView> createState() => _ControlViewState();
+  _ControlViewState createState() => _ControlViewState();
 }
 
 class _ControlViewState extends State<ControlView> {
   @override
   Widget build(BuildContext context) {
-    // Widget _notification(){
-    //   Widget _item(String image, String title){
-    //
-    //   }
-    // }
-
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              CustomAppBar(),
-            ],
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            // children: [
-            // ],
+      body: Column(
+        children: [
+          CustomAppBar(), // 기존에 있던 CustomAppBar 위젯
+          NoticeView(), // 공지사항 뷰를 추가
+          Expanded(
+            flex: 2,
+            child: TopMenuView(), // 상단 메뉴 뷰
           ),
-        )
-      )
+          Expanded(
+            flex: 3,
+            child: BoardView(), // 게시판 뷰
+          ),
+        ],
+      ),
     );
   }
 }
-
