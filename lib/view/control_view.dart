f78787030e8f0d9cd84ca23894ab305936582475
hiyseo <yoonseo@gim-yunseoui-MacBookPro-3.local>
2024-01-08 01:28:27 +0900

    1/8 01:28

--- lib/view/control_view.dart
+++ lib/view/control_view.dart
@@ -12,11 +12,34 @@ class ControlView extends StatefulWidget {
 class _ControlViewState extends State<ControlView> {
   @override
   Widget build(BuildContext context) {
-    // Widget _notification(){
-    //   Widget _item(String image, String title){
-    //
-    //   }
-    // }
+    Widget _notification(){
+      Widget _item(String image, String title){
+        return Expanded(
+          flex: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0x7FADCFF8),
+              borderRadius: BorderRadius.circular(10)),
+            // shape: RoundedRectangleBorder(
+            //   borderRadius: BorderRadius.circular(32)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24.0),
+            child: Column(
+              children: [
+                //TODO 여기에 공지 내용 추가
+                Image.network(
+                  image,
+                  width: 60,
+                  height: 40,
+                )
+              ]
+            ),
+          ),
+          ),
+
+        );
+      }
+    }
 
     return Scaffold(
       body: SingleChildScrollView(
